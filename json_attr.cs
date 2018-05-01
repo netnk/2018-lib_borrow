ab57be7ed7f905765bf2d8952d107f860ec719b4
netnk <netnk0000@gmail.com>
2018-05-01 09:42:45 +0800

    Create json_attr.cs

---
+++ json_attr.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+
+public class Checkadmi
+{
+    public string field { get; set; }
+    public string field { get; set; }
+    public string field { get; set; }
+    public string field { get; set; }
+}
+
+public class Login
+{
+    public string field { get; set; }
+    public string field { get; set; }
+    public string field { get; set; }  //
+    public string field { get; set; }  //
+    public string field { get; set; }  //
+    public string field { get; set; }  //
+    public int field { get; set; }  //
+    public float field { get; set; }  //
+
+    public int tsk { get; set; }  //
+    public int tsy { get; set; }  //
+    public int fsk { get; set; }  //
+    public int fsy { get; set; }  //
+    public int qkk { get; set; }  //
+    public int qky { get; set; }  //
+}
+
+
+public class Borrow
+{
+    public string field { get; set; }
+    public string field { get; set; }
+    public string field { get; set; }  //
+    public string field { get; set; }  //
+    public string field { get; set; }  //
+    public string field { get; set; }  //
+    public string field { get; set; }  //
+}
