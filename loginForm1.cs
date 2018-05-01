f0480aa515b7366a315b3922e02c4350bac5f8c6
netnk <netnk0000@gmail.com>
2018-05-01 10:24:44 +0800

    Update loginForm1.cs

--- loginForm1.cs
+++ loginForm1.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Net;
-using System.IO;
 using System.Windows.Forms;
 using Newtonsoft.Json;
+using lib_borrow;
 
 namespace lib_borrow
 {
     public partial class loginForm1 : Form
     {
+        lib_borrow.all_setting libhp = new all_setting();
+
         public loginForm1()
         {
             InitializeComponent();
@@ -21,30 +20,8 @@ namespace lib_borrow
             pictureBox1.BackColor = Color.Transparent;           
 
         }
-        
-        private string ini_url()
-        {
-            using (FileStream fs = new FileStream("host.ini", FileMode.Open))
-            {
-                using (StreamReader rw = new StreamReader(fs))
-                {
-                    string result = rw.ReadLine();
-                    return result;
-                }
-            }
-        }  
 
-        private string getservice(string url)
-        {
-            using (WebClient wc = new WebClient())
-            {
-                wc.Encoding = System.Text.Encoding.UTF8;
-                string result = wc.DownloadString(url);
-                return result;
-            }
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)  
+        private void pictureBox1_Click(object sender, EventArgs e)  //URL
         {
             Form2 frm = new Form2();
             frm.StartPosition = FormStartPosition.CenterScreen;            
@@ -54,21 +31,21 @@ namespace lib_borrow
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-            string ip = ini_url();
+            string field = libhp.ini_opacurl();
             string id = textBox1.Text.Trim();
             string pwd = textBox2.Text.Trim();
-            string url = string.Format("http://{0}/getservice.ashx?mode=1&userid={1}&pwd={2}", ip, id, pwd);
-            string get = getservice(url);
+            string url = string.Format("http://{0}/getservice.ashx?mode=1&userid={1}&pwd={2}", field, id, pwd);
+            string get = libhp.getservice(url);
 
             Checkadmi m = JsonConvert.DeserializeObject<Checkadmi>(get);
 
-            switch (m.result.ToString())
+            switch (m.field.ToString())
             {
                 case "0":
                     {
                         //  MessageBox.Show("Success!!");
                         Form1 frm1 = new Form1();
-                        frm1.String1 = m.userid;
+                        frm1.String1 = m.field;
                         frm1.SetValue();
                         frm1.ShowDialog();
                         this.Enabled = false;
