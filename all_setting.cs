07110618c2bad98e8a2cdeadb2bfbdde3c19dbee
netnk <netnk0000@gmail.com>
2018-05-02 13:06:21 +0800

    Update all_setting.cs

--- all_setting.cs
+++ all_setting.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.IO;
+using System.Data;
 using System.Windows.Forms;
 
 namespace lib_borrow
@@ -22,7 +23,7 @@ namespace lib_borrow
             }
         }  
 
-        public string write_url(string str)  //寫入INI檔用
+        public string write_url(string str)  //
         {
             using (FileStream fs = new FileStream("host.ini", FileMode.Create))
             {
@@ -44,7 +45,7 @@ namespace lib_borrow
             }
         }  
 
-        public void clean_control(params Control[] ctols)  //切換畫面時清除控制項
+        public void clean_control(params Control[] ctols)  //
         {
 
             //Reference
@@ -52,16 +53,39 @@ namespace lib_borrow
 
             foreach (Control ctol in ctols)
             {
-                if (ctol.Name == "control_name" || ctol.Name == "control_name" || ctol.Name == "control_name" || ctol.Name == "control_name" || ctol.Name == "control_name" || ctol.Name == "control_name")
+                if (ctol.Name == "lbl_tsk" || ctol.Name == "lbl_tsy" || ctol.Name == "lbl_fsk" || ctol.Name == "lbl_fsy" || ctol.Name == "lbl_qkk" || ctol.Name == "lbl_qky")
                 {
                     ctol.Text = "0";
-                }
+                }                
                 else
                 {
                     ctol.Text = string.Empty;
                 }
                
             }       
+        }      
+
+        public string write_log(string str)  //
+        {
+            using (FileStream fs = new FileStream("log.txt", FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.WriteLine(str);
+                    return string.Empty;
+                }
+            }
+        }
+
+        public void display_control(params Control[] ctols)  //
+        {
+            //Reference
+            //https://dotblogs.com.tw/kyleshen/2013/10/10/123562
+
+            foreach (Control ctol in ctols)
+            {
+                ctol.Visible = true;
+            }
         }
 
     }
