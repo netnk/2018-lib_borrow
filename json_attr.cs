735d9213085cbb02baf931530866964ca7b0f66a
netnk <netnk0000@gmail.com>
2018-06-22 23:46:59 +0800

    Update json_attr.cs

--- json_attr.cs
+++ json_attr.cs
@@ -2,24 +2,32 @@ using System;
 using System.Collections.Generic;
 
 
+public class SetHost
+{
+    public string mode { get; set; }
+    public string host { get; set; }
+    public string client { get; set; }
+    
+}
+
 public class Checkadmi
 {
-    public string field { get; set; }
-    public string field { get; set; }
-    public string field { get; set; }
-    public string field { get; set; }
+    public string result { get; set; }
+    public string msg { get; set; }
+    public string userid { get; set; }
+    public string location { get; set; }
 }
 
 public class Login
 {
-    public string field { get; set; }
-    public string field { get; set; }
-    public string field { get; set; }  //
-    public string field { get; set; }  //
-    public string field { get; set; }  //
-    public string field { get; set; }  //
-    public int field { get; set; }  //
-    public float field { get; set; }  //
+    public string result { get; set; }
+    public string msg { get; set; }
+    public string reader01 { get; set; }  
+    public string reader02 { get; set; }  
+    public string reader72 { get; set; }  //
+    public string yxrq { get; set; }  //
+    public int yyrgnum { get; set; }  //
+    public float fk { get; set; }  //
 
     public int tsk { get; set; }  //
     public int tsy { get; set; }  //
@@ -32,21 +40,36 @@ public class Login
 
 public class Borrow
 {
-    public string field { get; set; }
-    public string field { get; set; }
-    public string field { get; set; }  //
-    public string field { get; set; }  //
-    public string field { get; set; }  //
-    public string field { get; set; }  //
-    public string field { get; set; }  //
+    public string result { get; set; }
+    public string msg { get; set; }
+    public string datatype { get; set; }  //
+    public string acce01 { get; set; }  //
+    public string cata12 { get; set; }  //
+    public string sent01 { get; set; }  //
+    public string sent02 { get; set; }  //
+}
+
+public class Book_return
+{
+    public string result { get; set; }
+    public string msg { get; set; }
+    public string cata11 { get; set; }  //，, , 
+    public string acce01 { get; set; }  //
+    public string cata12 { get; set; }  //
+    public string sacce48 { get; set; }  //
+    public string sent02 { get; set; }  //
+    public string hist01 { get; set; }  //
+    public string overday { get; set; }  //
+    public string hist03 { get; set; }  //
+    public string sord04 { get; set; }  //
 }
 
 
 public class Book_hist
 {
-    public string field { get; set; }
-    public string field { get; set; }
-    public string field { get; set; }
-    public string field { get; set; }
-    public string field { get; set; }
+    public string acce01 { get; set; }
+    public string hcata12 { get; set; }
+    public string cata13 { get; set; }
+    public string sent01 { get; set; }
+    public string hist01 { get; set; }
 }
