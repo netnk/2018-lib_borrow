602d08549834ce195d708fc2de31da9814432add
netnk <netnk0000@gmail.com>
2018-06-22 23:54:58 +0800

    Create workingForm1.cs

---
+++ workingForm1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lib_borrow
+{
+    public partial class workingForm1 : Form
+    {
+        public workingForm1()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
